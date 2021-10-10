5b4be63d6ae65ff324cb28f32923c2055715bd86
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-10 10:14:44 +0300

    3. Headfirst. Jukebox

---
+++ src/main/java/ru.devopshelp/headfirst/collections/Jukebox.java
@@ -0,0 +1,37 @@
+package headfirst.collections;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Jukebox {
+    private ArrayList<String> songList = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        new Jukebox().go();
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+    }
+
+    void getSongs() {
+        try {
+            File file = new File("C:/SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
+}
