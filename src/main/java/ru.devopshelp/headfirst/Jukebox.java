9e9764adfc713e0bf1d815eb2acdcec287fff7fb
Evgeny Koregin <mnemonic78@yandex.ru>
2021-10-20 21:53:33 +0300

    2.5. Ферма

--- src/main/java/ru.devopshelp/headfirst/Jukebox.java
+++ src/main/java/ru.devopshelp/headfirst/Jukebox.java
@@ -1,4 +1,4 @@
-package ru.devopshelp.headfirst;
+package headfirst;
 
 import java.io.BufferedReader;
 import java.io.File;
--- src/main/java/ru.devopshelp/headfirst/Song.java
+++ src/main/java/ru.devopshelp/headfirst/Song.java
@@ -1,4 +1,4 @@
-package ru.devopshelp.headfirst;
+package headfirst;
 
 public class Song implements Comparable<Song> {
     String title;
